bdfa57c5e87d52b659c9e50839d64410c22cdde8
milad rezai <miladrezaix@gmail.com>
2025-06-05 14:47:46 +0330

    completed  ui

---
+++ index.php
@@ -0,0 +1,26 @@
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <div class="box">
+        <div class="exporterTitle"><h2>EXPORTER</h2></div>
+        <div class="form">
+            <form action="" method="post">
+                <input type="text" name="title" id="title" placeholder="title">
+                <textarea name="content" id="" placeholder="content"></textarea>
+                <select name="format" id="">
+                    <option value="text">text File</option>
+                    <option value="pdf">pdf File</option>
+                    <option value="json">json File</option>
+                    <input type="submit" value="Export to file" name="" id="submit">
+                 </select>
+             </form>
+        </div>
+    </div>
+</body>
+</html>
